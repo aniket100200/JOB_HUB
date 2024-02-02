65afebdabeb19469e19390414b7abab2dc387a4c
Aniket <aniketkhangar459@gmail.com>
2024-02-02 13:52:37 +0530

    First Backend Commit

---
+++ TalentMatchPro/src/main/java/com/example/TalentMatchPro/TalentMatchProApplication.java
@@ -0,0 +1,13 @@
+package com.example.TalentMatchPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentMatchProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentMatchProApplication.class, args);
+	}
+
+}
---
+++ TalentMatchPro/src/test/java/com/example/TalentMatchPro/TalentMatchProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TalentMatchPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentMatchProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
